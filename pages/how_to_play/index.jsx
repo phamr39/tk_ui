7265f5d56b55edebafa14aa639e40e4b8b854f50
Hieu Pham <hieu.phamnt39@gmail.com>
2022-08-16 14:12:40 +0700

    update code

--- pages/how_to_play/index.jsx
+++ pages/how_to_play/index.jsx
@@ -4,7 +4,7 @@ import styles from './HowToPlay.module.scss';
 const LotteryGameInfo = {
     header: 'Lottery Game',
     title: 'Play lottery with 100 others',
-    description: 'You will play with 100 other players and get a chance to win a prize! The contract generates the winning number randomly when the 100th player joins. All the winner players who buy the lucky ticket will share the jackpot. After that, the prize will be sent immediately to the winner\'s wallet. The 100th player also got 0.5 NEAR as a calculation fee for his contract call. Your will deposite 1 NEAR for each number, each user can buy only one number in each lottery game. 10% of the total deposit will be returned to the dealer. If there are more than 1 winner, the prize will be shared. In case of no one got the prize, the contract will be automatically refunded 0.5 NEAR to the player who joined the lottery.',
+    description: 'You will play with 100 other players and get a chance to win a prize! The contract generates the winning number randomly when the 100th player joins. All the winner players who buy the lucky ticket will share the jackpot. After that, the prize will be sent immediately to the winner\'s wallet. The 100th player also got 0.5 NEAR as a calculation fee for his contract call. Your will deposit 1 NEAR for each number, each user can buy only one number in each lottery game. 10% of the total deposit will be returned to the dealer. If there is more than 1 winner, the prize will be shared. In case of no one got the prize, 80% of the prize will be added to the next time.',
     ref: '/lottery',
 };
 
